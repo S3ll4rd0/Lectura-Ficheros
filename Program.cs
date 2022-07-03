a63c4e88fa1dd48d929573478db7202605109a3a
Antonio Nicolás Salmerón Rubio <54323367+S3ll4rd0@users.noreply.github.com>
2022-07-03 23:14:36 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+public class lecturaFichero
+{
+    public static void Main(String[] args)
+    {
+        using (StreamReader sr = new StreamReader(@"F:\05.Internet\Google Drive\00.Proyectos\07.C#\Ejer4.6.1\bin\Debug\net6.0\tecleado.txt"))
+        {
+            string line;
+            int i = 0;
+
+            while (i < 3) 
+            {
+                line = sr.ReadLine();
+                Console.WriteLine(line);
+                i++;
+            }
+            sr.Close();
+        }
+    }
+}
